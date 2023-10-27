62f5b05dc0d5c4dbbcaa2fa581c8d3eaf3e78bfc
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-10-26 21:31:00 -0300

    Layout e no geral funcionalidade do menu mobile implementada, faltando apenas a implementação de um outsideClick

--- src/pages/User/Header/UserHeaderNav.jsx
+++ src/pages/User/Header/UserHeaderNav.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../context";
 import styles from "./styles.module.css";
-import { BiExit, BiImageAdd, BiBarChart, BiCategory } from "react-icons/bi";
+import { BiExit, BiImageAdd, BiBarChart, BiCategory, BiMenu } from "react-icons/bi";
 
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
+  const [mobile, setMobile] = React.useState(false);
+
+  function handleMobile() {
+    setMobile((previous) => !previous);
+  }
 
   return (
     <nav className={styles.nav}>
-      <ul>
+      <span className={`${styles.mobileToggler} ${styles.navItem}`} onClick={handleMobile}>
+        <BiMenu className={`${styles.icons} ${styles.mobileToggler}`} />
+      </span>
+      <ul className={mobile ? `${styles.active}` : ""}>
         <li>
           <NavLink to={"/conta"} end className={styles.navItem}>
             <BiCategory className={styles.icons} />
