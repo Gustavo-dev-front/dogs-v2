a08bac1556dc3b2c8d17ca39dd6d7a173e3e98f9
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-10-30 22:16:37 -0300

    Requisição da Photo implementada no Modal. Agora falta inserir a lógica para exibição e envio de comentários

--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import Error from "../Helper/Error/Error";
+import useFetch from "../../hooks/useFetch";
+import Loading from "../Helper/Loading/Loading";
+import Title from "../../components/Title/Title";
+import { PHOTO_GET } from "../../api_endpoints";
 import styles from "./styles.module.css";
 
-const Photo = ({data}) => {
-  return <article className={styles.photo}>
-    <div className={styles.image}>TESTA</div>
-    <div className="content"></div>
-  </article>;
+const Photo = ({ photoId }) => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getPhoto() {
+      const { url, options } = PHOTO_GET(photoId);
+      const { response, json } = await request(url, options);
+      console.log(json);
+    }
+    if (photoId) getPhoto();
+  }, [photoId, request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (!data) return null;
+  return (
+    <article className={styles.photo}>
+      <div className={styles.image}>
+        <img src={data.photo.src} alt={data.photo.title} />
+      </div>
+      <div className={styles.content}>
+        <span className={styles.author}>@{data.photo.author}</span>
+        <span className={styles.access}>{data.photo.acessos}</span>
+        <div className={styles.details}>
+          <Title modal={true}>{data.photo.title}</Title>
+          <span>{data.photo.peso} kg</span>
+          <span>{data.photo.idade} anos</span>
+        </div>
+        <div className={styles.comments}>
+          <div className={styles.messages}></div>
+          <textarea placeholder="Comente..."></textarea>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Photo;
--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import Loading from "../../components/Helper/Loading/Loading";
 import Error from "../../components/Helper/Error/Error";
 import useFetch from "../../hooks/useFetch";
-import { PHOTOS_GET } from "../../api_endpoints";
+import { PHOTOS_GET, PHOTO_GET } from "../../api_endpoints";
 import Modal from "../../components/Modal";
 import Photo from "../../components/Photo/index";
 import styles from "./styles.module.css";
 
 const Feed = () => {
   const { data, loading, error, request } = useFetch();
+  const [photoId, setPhotoId] = React.useState(null); 
   const [modalDisplay, setModalDisplay] = React.useState(false);
 
   React.useEffect(() => {
     const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
     request(url, options);
   }, [request]);
-
-  function handleModal(e) {
+  
+  function handleModal(id) {
     setModalDisplay(true);
-  }
+    setPhotoId(id);
+  } 
 
   if (loading) return <Loading />;
   if (error) return <Error error={error} />;
@@ -26,12 +28,12 @@ const Feed = () => {
   return (
     <>
       <Modal active={modalDisplay} setModalDisplay={setModalDisplay}>
-        <Photo />
+        <Photo photoId={photoId}/>
       </Modal>
       <section className={`${styles.container} ${styles.feed}`}>
         <ul>
           {data.map(({ id, acessos, src, title }) => (
-            <li key={id} onClick={handleModal}>
+            <li key={id} onClick={() => handleModal(id)}>
               <img src={src} alt={title} />
               <span>{acessos}</span>
             </li>
