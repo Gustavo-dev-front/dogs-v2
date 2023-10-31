428853ad0ac4fc6ca85a04fabba570783c293c18
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-10-31 20:59:13 -0300

    Comments implementado, faltando a estilização do container das mensagens para que o mesmo respeite o tamanho quando a quantidade de mensagens crescer

---
+++ src/components/Photo/Comments/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ReactComponent as Enviar } from "../../../assets/enviar.svg";
+import useFetch from "../../../hooks/useFetch";
+import { COMMENT_POST } from "../../../api_endpoints";
+import Error from "../../Helper/Error/Error";
+import styles from "./styles.module.css";
+
+const Comments = (props) => {
+  const [comment, setComment] = React.useState();
+  const [comments, setComments] = React.useState(props.comments);
+  const { error, request } = useFetch();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const { url, options } = COMMENT_POST(props.id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) setComments((prev) => [...prev, json]);
+    setComment("");
+  }
+
+  return (
+    <div className={styles.comments}>
+      <ul className={styles.messages}>
+        {comments.map(({ comment_ID, comment_author, comment_content }) => {
+          return (
+            <li key={comment_ID}>
+              <span className={styles.author_comment}>{comment_author}:</span>
+              <span className={styles.comment}>{comment_content}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.send}>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            placeholder="Comente..."
+            value={comment}
+            onChange={({ target }) => setComment(target.value)}
+          ></textarea>
+          <button>
+            <Enviar />
+          </button>
+        </form>
+      </div>
+      {error && <Error error={error} />}
+    </div>
+  );
+};
+
+export default Comments;
--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -5,6 +5,7 @@ import Loading from "../Helper/Loading/Loading";
 import Title from "../../components/Title/Title";
 import { PHOTO_GET } from "../../api_endpoints";
 import styles from "./styles.module.css";
+import Comments from "./Comments/index";
 
 const Photo = ({ photoId }) => {
   const { data, loading, error, request } = useFetch();
@@ -12,8 +13,7 @@ const Photo = ({ photoId }) => {
   React.useEffect(() => {
     async function getPhoto() {
       const { url, options } = PHOTO_GET(photoId);
-      const { response, json } = await request(url, options);
-      console.log(json);
+      await request(url, options);
     }
     if (photoId) getPhoto();
   }, [photoId, request]);
@@ -27,17 +27,16 @@ const Photo = ({ photoId }) => {
         <img src={data.photo.src} alt={data.photo.title} />
       </div>
       <div className={styles.content}>
-        <span className={styles.author}>@{data.photo.author}</span>
-        <span className={styles.access}>{data.photo.acessos}</span>
+        <div className={styles.info}>
+          <span className={styles.author}>@{data.photo.author}</span>
+          <span className={styles.access}>{data.photo.acessos}</span>
+        </div>
         <div className={styles.details}>
           <Title modal={true}>{data.photo.title}</Title>
           <span>{data.photo.peso} kg</span>
           <span>{data.photo.idade} anos</span>
         </div>
-        <div className={styles.comments}>
-          <div className={styles.messages}></div>
-          <textarea placeholder="Comente..."></textarea>
-        </div>
+        <Comments comments={data.comments} id={data.photo.id}/>
       </div>
     </article>
   );
