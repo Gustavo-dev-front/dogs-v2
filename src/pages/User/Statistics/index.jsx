721fced2e8d20b23307a6757e6ccd328b42e66ed
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-11-06 21:37:16 -0300

    Inicio da página de estatisticas

--- src/pages/User/Statistics/index.jsx
+++ src/pages/User/Statistics/index.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import useFetch from "../../../hooks/useFetch";
+import { STATS_GET } from "../../../api_endpoints";
+import Loading from "../../../components/Helper/Loading/Loading";
+import Error from "../../../components/Helper/Error/Error";
 import styles from "./styles.module.css";
 
-const index = () => {
-  return <div>ESTATISTICAS</div>;
+const Statistics = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = STATS_GET();
+    request(url, options);
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (!data) return null;
+  return (
+    <section className={styles.statistics}>
+      <div className={styles.container}>
+        <span>
+          Acessos:{" "}
+          {data.reduce((previous, current) => previous + Number(current.acessos), 0)}
+        </span>
+      </div>
+      <div className={styles.container}>Gráfico 1</div>
+      <div className={styles.container}>Gráfico 2</div>
+    </section>
+  );
 };
 
-export default index;
+export default Statistics;
