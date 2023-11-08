7c3010d869b902bb43b9e3cdd17298f44ae32210
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-11-07 21:08:06 -0300

    Página de estatisticas implementada, mas falta ajustar melhor o posicionamento dos gráficos

--- src/pages/User/Statistics/index.jsx
+++ src/pages/User/Statistics/index.jsx
@@ -4,9 +4,18 @@ import { STATS_GET } from "../../../api_endpoints";
 import Loading from "../../../components/Helper/Loading/Loading";
 import Error from "../../../components/Helper/Error/Error";
 import styles from "./styles.module.css";
+import { VictoryChart, VictoryBar, VictoryPie } from "victory";
 
 const Statistics = () => {
   const { data, loading, error, request } = useFetch();
+  const processed_data = data?.map((item) => {
+    return { x: item.title, y: +item.acessos };
+  });
+  const acessos = processed_data?.reduce((previous, { y }) => {
+    return previous + Number(y);
+  }, 0);
+
+  console.log(processed_data);
 
   React.useEffect(() => {
     const { url, options } = STATS_GET();
@@ -19,13 +28,31 @@ const Statistics = () => {
   return (
     <section className={styles.statistics}>
       <div className={styles.container}>
-        <span>
-          Acessos:{" "}
-          {data.reduce((previous, current) => previous + Number(current.acessos), 0)}
-        </span>
+        <span>Acessos:{acessos}</span>
       </div>
-      <div className={styles.container}>Gráfico 1</div>
-      <div className={styles.container}>Gráfico 2</div>
+      <div className={styles.container}>
+        <VictoryChart>
+          <VictoryBar
+            data={processed_data}
+            alignment="middle"
+            labels={({ datum }) => datum.y}
+          />
+        </VictoryChart>
+      </div>
+      <div className={styles.container}>
+      <VictoryChart>
+          <VictoryPie
+            data={processed_data}
+            labels={({ datum }) => datum.y}
+            padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+            style={{
+              data: { fillOpacity: 0.9, stroke: "#fff", strokeWidth: 2 },
+              labels: { fontSize: 14, fill: "#333" },
+            }}
+          />
+          
+        </VictoryChart>
+      </div>
     </section>
   );
 };
